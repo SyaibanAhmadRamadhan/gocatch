dd2494c6ae45bcfe1633bad5f0d73ed70c90710c
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-12-17 09:52:04 +0700

    added db param

--- ginfra/gdb/db_param.go
+++ ginfra/gdb/db_param.go
@@ -4,6 +4,16 @@ import (
 	"github.com/SyaibanAhmadRamadhan/gocatch/garray"
 )
 
+type PaginationParam struct {
+	Limit  int64
+	Offset int64
+}
+
+type FindByOneColumnParam struct {
+	Column string
+	Value  any
+}
+
 type OrderBy struct {
 	Column      string
 	IsAscending bool
